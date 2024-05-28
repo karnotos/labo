b8960553de9db0f66206ba32d4b4dbe598ec2fca
notos <165032317+karnotos@users.noreply.github.com>
2024-05-28 18:11:42 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-4f476095-c623-4874-bd5c-3bd05d61707e/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-2d78ec2e-f87b-4b80-a05b-a66ed07bbad0/)
 
 
 echo "$curl_output" >> Update.tsx
